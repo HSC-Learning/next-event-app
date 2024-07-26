18d865a571a2e323eceab2d0d3e7c9e50905c5d8
HSC-Learning <hsc011010@gmail.com>
2024-07-26 13:27:01 +0200

    Update-1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Next-Event-App</h1>
       <Button>Hello</Button>
     </main>
   );
