41b32b261af0e54f44b9c113ddb528da29c07258
HSC-Learning <hsc011010@gmail.com>
2024-07-30 16:08:54 +0200

    Debugging - middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,47 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: [
-    "/",
-    "/events/:id",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-  ],
-  ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-  ],
+// export default clerkMiddleware({
+//   publicRoutes: [
+//     "/",
+//     "/events/:id",
+//     "/api/webhook/clerk",
+//     "/api/webhook/stripe",
+//     "/api/uploadthing",
+//   ],
+//   ignoredRoutes: [
+//     "/api/webhook/clerk",
+//     "/api/webhook/stripe",
+//     "/api/uploadthing",
+//   ],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isTenantRoute = createRouteMatcher([
+  "/organization-selector(.*)",
+  "/orgid/(.*)",
+]);
+
+const isTenantAdminRoute = createRouteMatcher([
+  "/orgId/(.*)/memberships",
+  "/orgId/(.*)/domain",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // Restrict admin routes to users with specific permissions
+  if (isTenantAdminRoute(req)) {
+    auth().protect((has) => {
+      return (
+        has({ permission: "org:sys_memberships:manage" }) ||
+        has({ permission: "org:sys_domains_manage" })
+      );
+    });
+  }
+  // Restrict organization routes to signed in users
+  if (isTenantRoute(req)) auth().protect();
 });
 
 export const config = {
